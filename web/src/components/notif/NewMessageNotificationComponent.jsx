07404856cbe44f69ce9626e2fc109d9cdd980deb
Thariq Alfa <thariq.benriska@mandirisekuritas.co.id>
2024-08-08 12:19:01 +0700

    Update NewMessageNotificationComponent.jsx

--- web/src/components/notif/NewMessageNotificationComponent.jsx
+++ web/src/components/notif/NewMessageNotificationComponent.jsx
@@ -25,7 +25,7 @@ const NewMessageNotificationComponent = ({ isShow }) => {
         setTimeout(() => {
           setNotificationMessage({ type: "" });
         }, 1000);
-      }, 5000);
+      }, 3000);
     }
   }, [isClose]);
 
