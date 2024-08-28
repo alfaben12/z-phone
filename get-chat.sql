45f9168ecd99c0e6d68439b6a4c7e10da726535d
Thariq Alfa <thariq.benriska@mandirisekuritas.co.id>
2024-08-28 17:37:36 +0700

    Create get-chat.sql

---
+++ get-chat.sql
@@ -0,0 +1,46 @@
+WITH LatestMessages AS (
+    SELECT
+        `conversationid`,
+        `content`,
+        `created_at`,
+        ROW_NUMBER() OVER (PARTITION BY `conversationid` ORDER BY `created_at` DESC) AS rn
+    FROM
+        `zp_conversation_messages`
+)
+SELECT
+	from_user.`avatar`,
+    CASE
+        WHEN c.`is_group` = 0 THEN
+            COALESCE(
+                contact.`contact_name`,
+                from_user.`phone_number`
+            )
+        ELSE c.`name`
+    END AS `conversation_name`,
+    from_user.`last_seen`,
+    0 as isRead,
+    last_msg.`content` AS `last_message`,
+    last_msg.`created_at` AS `last_message_time`
+FROM
+    `zp_conversations` c
+JOIN
+    `zp_conversation_participants` p
+    ON c.`conversationid` = p.`conversationid`
+LEFT JOIN
+    `zp_conversation_participants` other_participant
+    ON c.`conversationid` = other_participant.`conversationid`
+    AND other_participant.`citizenid` != p.`citizenid`
+LEFT JOIN
+    `zp_users` from_user
+    ON other_participant.`citizenid` = from_user.`citizenid`
+LEFT JOIN
+    `zp_contacts` contact
+    ON contact.`citizenid` = p.`citizenid`
+    AND contact.`contact_citizenid` = other_participant.`citizenid`
+LEFT JOIN
+    LatestMessages last_msg
+    ON c.`conversationid` = last_msg.`conversationid` AND last_msg.rn = 1
+WHERE
+    p.`citizenid` = 'citizen001'
+ORDER BY
+    c.`created_at` DESC;
