1a036e89570243ad0dc7e3afbfe046cba70126d3
GyozaLion <115346313+GyozaLion@users.noreply.github.com>
2024-10-06 20:24:55 +0700

    Update garages.lua
    Fixed Some issue for people who's using older version or Jdev version of qb-garages

--- server/garages.lua
+++ server/garages.lua
@@ -4,17 +4,18 @@ lib.callback.register('z-phone:server:GetVehicles', function(source, body)
     local Player = QBCore.Functions.GetPlayer(source)
     if Player ~= nil then
         local citizenid = Player.PlayerData.citizenid
+
+        -- Adjusted SQL query: Removed `created_at`
         local query = [[
             select 
-                pv.vehicle,
-                pv.plate,
-                pv.garage,
-                pv.fuel,
-                pv.engine,
-                pv.body,
-                pv.state,
-                DATE_FORMAT(pv.created_at, '%d %b %Y %H:%i') as created_at
-            from player_vehicles pv WHERE pv.citizenid = ? order by plate asc
+                vehicle,
+                plate,
+                garage,
+                fuel,
+                engine,
+                body,
+                state
+            from player_vehicles WHERE citizenid = ? order by plate asc
         ]]
 
         local result = MySQL.query.await(query, {
